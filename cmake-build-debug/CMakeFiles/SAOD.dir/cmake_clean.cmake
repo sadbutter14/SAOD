79ba2d87fb377e90e9d0b1e5fae9cced6f26eed4
Maxim <maslomaxx@gmail.com>
2019-02-14 00:41:14 +0700

    lab1

---
+++ cmake-build-debug/CMakeFiles/SAOD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SAOD.dir/SAOD1.cpp.o"
+  "SAOD.pdb"
+  "SAOD"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SAOD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TRPO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TRPO.dir/SAOD1.cpp.o"
+  "TRPO.pdb"
+  "TRPO"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TRPO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
